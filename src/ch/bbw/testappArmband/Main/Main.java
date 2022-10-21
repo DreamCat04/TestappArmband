c43803247665fbae081625ea1b549d69ebe8d8ce
thierry-kellenberger <74236102+thierry-kellenberger@users.noreply.github.com>
2022-10-21 17:53:43 +0200

    Changes made from 2h call

--- src/ch/bbw/testappArmband/Main/Main.java
+++ src/ch/bbw/testappArmband/Main/Main.java
@@ -4,7 +4,7 @@ import ch.bbw.testappArmband.Decoder.Decoder;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 		Decoder dec = new Decoder();
+		//dec.decode();
 	}
 }
---
+++ src/ch/bbw/testappArmband/Vaildator/Validator.java
@@ -0,0 +1,5 @@
+package ch.bbw.testappArmband.Vaildator;
+
+public class Validator {
+// The validator checks if a code is valid and meets the definition.
+}
