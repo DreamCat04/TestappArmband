8141efa67f579c4936d1833d91179f998aabb673
thierry-kellenberger <74236102+thierry-kellenberger@users.noreply.github.com>
2022-10-21 18:04:31 +0200

    Changes by janni

--- src/ch/bbw/testappArmband/Decoder/Decoder.java
+++ src/ch/bbw/testappArmband/Decoder/Decoder.java
@@ -1,13 +1,55 @@
 package ch.bbw.testappArmband.Decoder;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Decoder {
-	public Decoder() {
-		String out = "";
+	// The decoder should decode all the code blocks
+	public Decoder() { //Constructor
+		HashMap<String, String> festivalNames = new HashMap<String, String>();
+		HashMap persons = new HashMap<>();
+		HashMap days = new HashMap<String, String>();
+		HashMap<Integer, String> zones = new HashMap<Integer, String>();
+		
+		festivalNames.put("FO", "Frauenfeld Openair");
+		festivalNames.put("SO", "St.Gallen Openair");
+		festivalNames.put("OG", "Openair Gampel");
+		festivalNames.put("OZ", "Openair Zürich");
+		
+		days.put(1, "Donnerstag");
+		days.put(2, "Freitag");
+		days.put(3, "Do & Fr");
+		days.put(4, "Samstag");
+		days.put(5, "Do & Sa");
+		days.put(6, "Fr & Sa");
+		days.put(7, "Do & Sa");
+		days.put(8, "Sonntag");
+		days.put(9, "Do & So");
+		days.put("A", "Fr & So");
+		days.put("B", "Do, Fr, So");
+		days.put("C", "Sa & So");
+		days.put("D", "Do, Sa, So");
+		days.put("E", "Fr - So");
+		days.put("F", "Alle Tage");
+		
+		
+		
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Geben Sie hier ihren Code ein: ");
-		out = keyboard.next();
-		System.out.println(out);
+		String code = keyboard.next();
+		if(code.length() != 18) {
+			System.out.println("Sie haben einen ungültigen Code eingeben.");
+			
+		} else {
+		String[] splitCode = code.split("-");
+		String location = splitCode[0];
+		//Hexadecimal to decimal
+		int id = Integer.parseInt(splitCode[1], 16);
+		
+		
+		
+		
+		}
 	}
+	
 }
