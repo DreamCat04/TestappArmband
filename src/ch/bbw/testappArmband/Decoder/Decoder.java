0e926049a8f0480ea902e181485115c135c60d21
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2022-10-22 14:56:36 +0200

    I worked on the validation part...

--- src/ch/bbw/testappArmband/Decoder/Decoder.java
+++ src/ch/bbw/testappArmband/Decoder/Decoder.java
@@ -10,14 +10,14 @@ public class Decoder {
 		// These HashMaps are used for decoding
 		HashMap<String, String> festivalNames = new HashMap<String, String>();
 		HashMap persons = new HashMap<>();
-		HashMap <String, String> days = new HashMap<String, String>();
+		HashMap<String, String> days = new HashMap<String, String>();
 		HashMap<Integer, String> zones = new HashMap<Integer, String>();
-		
+
 		festivalNames.put("FO", "Frauenfeld Openair");
 		festivalNames.put("SO", "St.Gallen Openair");
 		festivalNames.put("OG", "Openair Gampel");
 		festivalNames.put("OZ", "Openair Zürich");
-		
+
 		days.put("1", "Donnerstag");
 		days.put("2", "Freitag");
 		days.put("3", "Do & Fr");
@@ -33,24 +33,37 @@ public class Decoder {
 		days.put("D", "Do, Sa, So");
 		days.put("E", "Fr - So");
 		days.put("F", "Alle Tage");
-		
-		
-		
+
+
+		boolean run = true;
+	while (run) {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Geben Sie hier ihren Code ein: ");
 		String code = keyboard.next();
-		if(code.length() != 18) {
-			System.out.println("Sie haben einen ungültigen Code eingeben.");
-		} else {
 		String[] splitCode = code.split("-");
-		String location = splitCode[0].substring(0, 2);
-		int year = Integer.parseInt(splitCode[0].substring(2,4));
+
+		//Validation of the Bar-code
+		if (code.length() != 18) {
+			System.out.println("Sie haben einen ungültigen Code eingeben.");
+			System.exit(0);
+		} else if (! splitCode[0].substring(0, 2).matches("(.*[A-Z].*)")) {
+			System.out.println("Sie haben einen ungültigen Code eingeben, die ersten beiden Stellen des Codes können nur Buchstaben sein.");
+		} else if (! splitCode[0].substring(2, 4).matches("(.*[0-9].*)")){
+			System.out.println("Sie haben einen ungültigen Code eingeben, die letzten Stellen des ersten Blocks können nur Zahlen sein.");
+		} else if () {
+
+		}
+
+		String location = festivalNames.get(splitCode[0].substring(0, 2));
+		int year = Integer.parseInt(splitCode[0].substring(2, 4));
 		//Hexadecimal to decimal
 		int id = Integer.parseInt(splitCode[1], 16);
 		String dates = splitCode[2];
 		String binaryZone = splitCode[3];
+	}
+
 
 		}
+
 	}
-	
 }
